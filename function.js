b1dee63fa932f17c1a84a3eaf6dfc3790e1c689e
montionproductions <idv15c.jmontion@uartesdigitales.edu.mx>
2023-04-18 00:13:14 -0600

    Remove column with setInterval()

---
+++ function.js
@@ -0,0 +1,18 @@
+let result;
+
+window.function = function(time){
+
+    day = day.value ?? "domingo";
+    hour = hour.value ?? "13:00"
+
+    let timerID;
+
+    timerID = setInterval(getResult, time);
+
+    return result;
+ }
+
+ function getResult() {
+    console.log("Se ha reiniciado la columan");
+    result = "";
+ }
